71fe757392cc9f93dd36e93759e52f88ca132e7a
TongYuan <595915539@qq.com>
2022-06-19 13:49:23 +0800

    加入创建数据库和表的各项配置。

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.coolweather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
